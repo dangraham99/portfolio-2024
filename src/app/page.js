6a0c2b6d6172fa4c9558f271d016ecc9a87399f7
Dan <36828197+dangraham99@users.noreply.github.com>
2024-01-21 19:42:15 +0000

    added educational section

--- src/app/page.js
+++ src/app/page.js
@@ -3,29 +3,79 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="  ">
-      <header className=' top-0 z-50 sticky py-4 backdrop-blur-lg mb-12 bg-stone-800 border-b bg-opacity-50 shadow-lg border-stone-700'>
-        <h1 className='text-2xl xl:w-1/2 md:w-2/3  mx-auto font-semibold'>Daniel Graham.</h1>
+      <header className='top-0 z-50 sticky py-4 backdrop-blur-lg mb-12 dark:bg-stone-800 border-b bg-opacity-100 dark:bg-opacity-40  dark:border-stone-700'>
+        <h1 className='text-xl xl:w-1/2 md:w-2/3 md:px-0 px-4 mx-auto font-semibold '>Daniel Graham.</h1>
       </header>
       <div class="mx-auto xl:w-1/2 md:w-2/3 px-8 min-h-screen">
 
-        <div className="my-8 tracking-tigher sm:text-xl">
-
-          <p className="font-semibold text-2xl  mb-6 leading-7">Hello, my name is Dan. I'm a <span className='text-red-400'>software and web developer.</span> </p>
-
-          <p className="leading-6">I'm a law graduate with an interest in software and web development. Currently I work with React and React Native. I've also enjoyed working with Flutter and SwiftUI.</p>
-          <p className='mt-4'>Let's work together!</p>
+        <div class="mb-24">
+          <div className="my-8 tracking-tigher">
+            <p className="font-semibold text-3xl  mb-6 leading-7">Hello, my name is Dan. I'm a <span className='text-red-400'>software and web developer.</span> </p>
+            <p className="leading-6">I'm a law graduate with an interest in software and web development. Currently I work with React and React Native. I've also enjoyed working with Flutter and SwiftUI.</p>
+            <p className='mt-4'>Let's work together!</p>
+          </div>
+          <div class="flex space-x-4  tracking-widest text-sm uppercase underline mt-2 font-regular text-red-400 ">
+            <p className="cursor-pointer">github</p>
+            <p className="cursor-pointer">twitter</p>
+            <p className="cursor-pointer">email</p>
+          </div>
         </div>
 
-        <div class="flex space-x-4  tracking-widest text-sm uppercase underline mt-2 font-regular text-red-400 ">
-          <p className="cursor-pointer">github</p>
-          <p className="cursor-pointer">twitter</p>
-          <p className="cursor-pointer">email</p>
+        <div className='flex md:flex-row flex-col space-y-4 md:space-y-0'>
+          <div class="sticky z-50 md:w-1/3">
+            <h1 className='font-semibold text-xl'>Education</h1>
+            <h2 className='uppercase font-semibold text-sm tracking-[.2em] text-stone-500'>My background</h2>
+          </div>
+          <div class="md:w-2/3">
+            <div className='mb-12'>
+              <h2 className='font-semibold text-red-400'>University of Glasgow, UK</h2>
+              <div className='mb-8'>
+                <div class="flex justify-between mb-4">
+                  <h2 className='font-semibold'>Diploma in Professional Legal Practise <br></br><i>Merit</i></h2>
+                  <h2>2021 - 2022</h2>
+                </div>
+                <div>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat iste impedit, vitae fugiat officiis alias nulla incidunt porro animi quam doloremque, enim ratione perspiciatis ad veniam aut sapiente qui error!
+                </div>
+              </div>
+
+              <div>
+                <div class="flex justify-between mb-4">
+                  <h2 className='font-semibold '>Law LLB (Hons.)<br></br><i>First class</i></h2>
+                  <h2>2017 - 2021</h2>
+                </div>
+                <div>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat iste impedit, vitae fugiat officiis alias nulla incidunt porro animi quam doloremque, enim ratione perspiciatis ad veniam aut sapiente qui error!
+                </div>
+              </div>
+
+            </div>
+
+            <div className='mb-12'>
+              <h2 className='font-semibold text-red-400'>Uppsala University, Sweden</h2>
+              <div className='mb-8'>
+                <div class="flex justify-between mb-4">
+                  <h2 className='font-semibold '>Erasmus+ Exchange Programme (Law) </h2>
+                  <h2>2019 - 2020</h2>
+                </div>
+                <div>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat iste impedit, vitae fugiat officiis alias nulla incidunt porro animi quam doloremque, enim ratione perspiciatis ad veniam aut sapiente qui error!
+                </div>
+              </div>
+
+              <div>
+
+              </div>
+
+            </div>
+
+
+
+
+
+          </div>
 
         </div>
-
-
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, dolor dicta similique sequi voluptatem quidem aliquid. Illo consequuntur deserunt expedita nisi modi aspernatur praesentium laudantium corrupti labore accusamus veritatis tenetur soluta quidem nobis aliquam doloribus sit suscipit doloremque nemo, aperiam esse fuga, veniam culpa provident! Quam sed neque quibusdam dolores aspernatur dolorem mollitia aut sunt reiciendis perferendis voluptatum assumenda incidunt magni nihil suscipit dicta illum, ipsa officia vero fuga accusamus placeat doloribus corporis! Enim repudiandae, aut facere ex ipsum doloribus? Totam unde excepturi, quos dolorum pariatur provident ut eaque architecto nam velit? Nostrum commodi debitis voluptate est dolorum suscipit! Reiciendis voluptas totam recusandae vero alias quos iusto perferendis minima corrupti dolor. Aspernatur aut eum accusamus dolore quibusdam voluptatem, optio similique numquam amet aperiam, veniam ab placeat! Quas nihil eaque, quis maiores ullam dolores distinctio rem? Saepe adipisci quasi nam necessitatibus id nostrum! Dicta sunt reprehenderit nam nobis maiores itaque labore nisi voluptatem necessitatibus blanditiis debitis officia ab, repellendus, ex corrupti. Dolore modi, praesentium beatae sint distinctio ipsam, enim excepturi non dicta asperiores facere quis neque. Porro ducimus facilis nesciunt doloribus consectetur qui tenetur suscipit quis ratione, nisi beatae quidem neque sint rerum, sequi consequatur. Delectus assumenda, cum possimus blanditiis dolore sunt id magni ab ex perferendis? Vel maxime, nostrum deleniti vitae harum culpa. Odit ullam at rem explicabo dignissimos aperiam cum voluptatem nemo nihil, quae laborum tempora quidem veniam, perspiciatis assumenda cumque. Dolorem itaque natus, officia, iusto porro maiores tenetur qui quos modi expedita quisquam voluptatibus est omnis voluptate. Ullam commodi quae, cumque aliquam delectus soluta. Aliquam nesciunt sequi in cumque rem aliquid eligendi ab libero culpa porro voluptatibus laudantium, a eius eaque autem velit, maxime ex ea pariatur rerum? Voluptate dolorum voluptas dignissimos quia omnis architecto dolores autem? Porro voluptatibus, deserunt tempora placeat perferendis sed mollitia excepturi dignissimos, laudantium, maiores commodi quaerat. Incidunt assumenda veniam debitis eveniet et provident error culpa doloribus labore necessitatibus officia hic laboriosam ipsam aliquid voluptatibus, ipsa iure tempora architecto nesciunt consequatur sint. Ipsum corrupti porro neque architecto fugit non quasi et maxime necessitatibus, totam obcaecati? Animi ex tempore quasi nostrum. Eius at minima ipsa iste praesentium. Quos velit molestiae iusto omnis! Quibusdam, dicta fugit. Rem quaerat dignissimos explicabo reprehenderit quia consectetur, nesciunt ab distinctio accusantium facere molestiae natus obcaecati illo possimus? Animi a ut obcaecati aut neque itaque consequatur corrupti facilis fugiat assumenda beatae debitis totam, reprehenderit natus tempora quod perferendis odio, architecto, nisi molestiae rerum ex. Veniam vitae aspernatur facere dolorem dolor voluptatem aliquid tempora non ipsam placeat provident quod porro atque sequi recusandae amet veritatis inventore, velit, earum doloremque magnam nisi. Distinctio provident sit doloribus laboriosam dolor minus libero molestias atque blanditiis obcaecati impedit ipsa repellat sunt omnis, maiores animi earum facilis odio in et. Modi quo consequatur in quaerat necessitatibus neque atque, doloremque obcaecati ab sunt assumenda dicta tenetur error cumque reiciendis voluptatibus suscipit laboriosam vero est nostrum odit culpa? Provident modi molestias odio repellat et ab, ipsam magni sunt sapiente quibusdam architecto. Nisi, autem minus temporibus ipsa harum dolore. Sed.
-
       </div>
 
 
