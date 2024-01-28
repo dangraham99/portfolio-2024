91984898be04e302c6d7b4af928d5c9ec4203103
Dan <36828197+dangraham99@users.noreply.github.com>
2024-01-28 22:36:45 +0000

    placeholder page

---
+++ src/app/blog/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Section from '@/components/home/Section'
+import Image from 'next/image'
+
+function Blog() {
+    return (
+        <Section title={"Blog"} subtitle={"Posts and updates"}>
+
+
+
+
+
+        </Section>
+    )
+}
+
+export default Blog
