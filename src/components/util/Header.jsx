72dd7db17228448495bf8ea98061dae013ad6c8c
Dan <36828197+dangraham99@users.noreply.github.com>
2024-01-28 22:22:28 +0000

    links

--- src/components/util/Header.jsx
+++ src/components/util/Header.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 
 
@@ -24,16 +25,22 @@ function Header() {
         <span>
 
 
-            <header className='top-0 z-50 sticky py-4 backdrop-blur-lg mb-12 dark:bg-stone-800 border-b bg-opacity-100 dark:bg-opacity-40 border-t overscroll-contain dark:border-stone-700'>
+            <header className='top-0 z-50 sticky py-4 backdrop-blur-lg mb-0 dark:bg-stone-800 border-b bg-opacity-100 dark:bg-opacity-40 border-t overscroll-contain dark:border-stone-700'>
                 <div className='flex justify-between items-center xl:w-1/2 md:w-2/3 md:px-0 px-4 mx-auto'>
-                    <h1 className='text-xl shrink-0 font-semibold '>Daniel Graham.</h1>
+                    <Link href="/">
+                        <h1 className='text-xl shrink-0 font-semibold '>Daniel Graham.</h1>
+                    </Link>
                     <div className='hidden shrink-0 sm:flex gap-6'>
-                        <a className='hover:cursor-pointer stroke-stone-400'>Contact</a>
-                        <a className='hover:cursor-pointer  stroke-stone-400'>Blog</a>
+                        <Link href="/contact">
+                            <p className='hover:cursor-pointer stroke-stone-400'>Contact</p>
+                        </Link>
+                        <Link href="/blog">
+                            <p className='hover:cursor-pointer  stroke-stone-400'>Blog</p>
+                        </Link>
                     </div>
                     <svg
                         onClick={() => { setIsMobileDialogOpen(!isMobileDialogOpen) }}
-                        xmlns="http://www.w3.org/2000/svg" className="flex shrink-0 sm:hidden h-8 w-8 stroke-stone-400 hover:cursor-pointer" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        xmlns="http://www.w3.org/2000/svg" className="shrink-0 flex sm:hidden h-8 w-8 stroke-stone-400 hover:cursor-pointer" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
 
@@ -43,29 +50,33 @@ function Header() {
             </header >
 
 
-            {isMobileDialogOpen &&
+            {
+                isMobileDialogOpen &&
 
                 <div onClick={() => { setIsMobileDialogOpen(!isMobileDialogOpen) }} className="fixed top-[4.1rem] z-60 backdrop-blur-2xl mb-12 dark:bg-stone-800  bg-opacity-100 dark:bg-opacity-40  dark:border-stone-700 h-screen w-screen bg-red-500  border-stone-700">
 
                     <div className="flex-col mt-12 px-8 text-lg">
                         <ul className='space-y-8'>
                             <li>
-                                <span className="flex hover:cursor-pointer w-max p-2">
-                                    About
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="ml-4 w-6 h-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                                    </svg>
-
-                                </span>
+                                <Link href="/contact">
+                                    <span className="flex hover:cursor-pointer w-max p-2">
+                                        Contact
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="ml-4 w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                                        </svg>
+                                    </span>
+                                </Link>
                             </li>
                             <li>
-                                <span className="flex hover:cursor-pointer w-max p-2">
-                                    Blog
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="ml-4 w-6 h-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                                    </svg>
+                                <Link href="/blog">
+                                    <span className="flex hover:cursor-pointer w-max p-2">
+                                        Blog
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="ml-4 w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                                        </svg>
 
-                                </span>
+                                    </span>
+                                </Link>
                             </li>
                         </ul>
 
