2dee6e13264790889c10b65049c91ba6786e042c
Dan <36828197+dangraham99@users.noreply.github.com>
2024-01-28 16:20:23 +0000

    fixed positioning on mobile header

--- src/components/util/Header.jsx
+++ src/components/util/Header.jsx
@@ -22,9 +22,30 @@ function Header() {
 
     return (
         <span>
+
+
+            <header className='top-0 z-50 sticky py-4 backdrop-blur-lg mb-12 dark:bg-stone-800 border-b bg-opacity-100 dark:bg-opacity-40 overscroll-contain dark:border-stone-700'>
+                <div className='flex justify-between items-center xl:w-1/2 md:w-2/3 md:px-0 px-4 mx-auto'>
+                    <h1 className='text-xl shrink-0 font-semibold '>Daniel Graham.</h1>
+                    <div className='hidden shrink-0 sm:flex gap-6'>
+                        <a className='hover:cursor-pointer stroke-stone-400'>Contact</a>
+                        <a className='hover:cursor-pointer  stroke-stone-400'>Blog</a>
+                    </div>
+                    <svg
+                        onClick={() => { setIsMobileDialogOpen(!isMobileDialogOpen) }}
+                        xmlns="http://www.w3.org/2000/svg" className="flex shrink-0 sm:hidden h-8 w-8 stroke-stone-400 hover:cursor-pointer" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+
+                </div>
+
+
+            </header >
+
+
             {isMobileDialogOpen &&
 
-                <div onClick={() => { setIsMobileDialogOpen(!isMobileDialogOpen) }} className="absolute top-[4.1rem] z-60 backdrop-blur-2xl mb-12 dark:bg-stone-800  bg-opacity-100 dark:bg-opacity-40  dark:border-stone-700 h-screen w-screen bg-red-500  border-stone-700">
+                <div onClick={() => { setIsMobileDialogOpen(!isMobileDialogOpen) }} className="fixed top-[4.1rem] z-60 backdrop-blur-2xl mb-12 dark:bg-stone-800  bg-opacity-100 dark:bg-opacity-40  dark:border-stone-700 h-screen w-screen bg-red-500  border-stone-700">
 
                     <div className="flex-col mt-12 px-8 text-lg">
                         <ul className='space-y-8'>
@@ -55,23 +76,6 @@ function Header() {
                 </div>
 
             }
-
-            <header className='top-0 z-50 sticky py-4 backdrop-blur-lg mb-12 dark:bg-stone-800 border-b bg-opacity-100 dark:bg-opacity-40 overscroll-contain dark:border-stone-700'>
-                <div className='flex justify-between items-center xl:w-1/2 md:w-2/3 md:px-0 px-4 mx-auto'>
-                    <h1 className='text-xl shrink-0 font-semibold '>Daniel Graham.</h1>
-                    <div className='hidden shrink-0 sm:flex gap-6'>
-                        <a className='hover:cursor-pointer stroke-stone-400'>Contact</a>
-                        <a className='hover:cursor-pointer  stroke-stone-400'>Blog</a>
-                    </div>
-                    <svg
-                        onClick={() => { setIsMobileDialogOpen(!isMobileDialogOpen) }}
-                        xmlns="http://www.w3.org/2000/svg" className="flex shrink-0 sm:hidden h-8 w-8 stroke-stone-400 hover:cursor-pointer" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-
-                </div>
-            </header >
-
 
         </span >
     )
