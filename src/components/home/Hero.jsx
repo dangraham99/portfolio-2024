b0aed7f6124b11285b7ed04d22e5ed7fdf0b3461
Dan <36828197+dangraham99@users.noreply.github.com>
2024-01-30 23:42:07 +0000

    updated about me

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 function Hero() {
     return (
         <div>
-            <div className="my-24 tracking-tigher">
-                <p className="mb-6 text-3xl font-semibold leading-10">Hello, my name is Dan. I'm a <span className='text-red-400'>software and web developer.</span> </p>
-                <div className="text-stone-400">
-                    <p className="leading-6">I'm a law graduate with an interest in software and web development. I enjoy creating intuitive digital experiences that are as engaging as they are seamless. Currently I work with React and React Native. I've also enjoyed working with Flutter and SwiftUI.</p>
+            <div className="my-24 tracking-tigher ">
+                <p className="mb-8 text-3xl font-semibold leading-10">Hello, my name is Dan. I'm a <span className='text-red-400'>software and web developer.</span> </p>
+                <div className="text-stone-400 text-justify pr-0 xl:pr-24">
+                    <p className="leading-6">I'm a law graduate and have been an indie developer for <span className='font-semibold text-red-400'>over 10 years</span>. I enjoy creating intuitive digital experiences that are as engaging as they are seamless. </p>
+                    <p className='mt-4'> Since 2018, while at university, I have been focused on using modern web technologies, starting with full-stack applications developed in Laravel before moving to <span className='text-red-400 font-semibold'>responsive, design-led, frontend projects</span> in Vue and React. I have been building my experience in Node.js as a lightweight backend and proxy for these projects.</p>
+                    <p className='mt-4'> During all of this I have also been exploring <span className='text-red-400 font-semibold'>native and cross-platform software development</span> in frameworks that interest me, like Swift and Flutter.</p>
                     <p className='mt-4'>Let's work together!</p>
                 </div>
             </div>
