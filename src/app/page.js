b38211bab45b46516586ee1a958316bee683981b
Dan <36828197+dangraham99@users.noreply.github.com>
2024-01-30 23:41:47 +0000

    removing CV from home; adding longer about me seciton as placeholder for projects section

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Section from '@/components/home/Section'
 import Header from '@/components/util/Header'
 import Hero from '@/components/home/Hero'
 import CvEntry from '@/components/home/CVEntry'
+import BentoBox from '@/components/util/BentoBox'
 
 export default function Home() {
 
@@ -16,6 +17,7 @@ export default function Home() {
 
       <div class="mb-24">
         <Hero />
+
       </div>
 
 
@@ -39,61 +41,9 @@ export default function Home() {
         </div>
       </div>
 
+      {/* <BentoBox /> */}
 
 
-      <Section title="Experience" subtitle="Previous roles">
-
-
-        <CvEntry
-          institution="Commercial law firm, Glasgow"
-          title="Trainee Solicitor"
-          period="2022 - Current"
-        />
-
-        <CvEntry
-          institution="Debt recovery law firm, Glasgow"
-          title="Court Department / Developer"
-          period="2016; 2019"
-        />
-
-      </Section>
-
-
-
-
-      <Section title="Education" subtitle="My background">
-
-        <CvEntry
-          institution="University of Glasgow, UK"
-          title="Diploma in Professional Legal Practise"
-          result="Merit"
-          period="2021 - 2022"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ratione beatae accusantium quibusdam cumque quod aspernatur. Tenetur temporibus possimus expedita maiores voluptatum maxime, facere velit earum repellendus consectetur doloribus voluptates."
-        />
-
-        <CvEntry
-          title="Law LLB (Hons.)"
-          period="2017 - 2021"
-          result="First class"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ratione beatae accusantium quibusdam cumque quod aspernatur. Tenetur temporibus possimus expedita maiores voluptatum maxime, facere velit earum repellendus consectetur doloribus voluptates."
-        />
-
-        <CvEntry
-          institution="Uppsala University, Sweden"
-          title="Erasmus+ Exchange Programme (Law)"
-          period="2019 - 2020"
-
-        />
-
-        <CvEntry
-          institution="Unnamed High School"
-          title="6x Scottish Highers; Advanced Higher Computing Science"
-          result="AAAAAAB"
-          period="2011 - 2017"
-
-        />
-
-      </Section>
 
 
 
