d860fc773c8b8b4c5c8eaf03a5aec0d0f949f483
Dan <36828197+dangraham99@users.noreply.github.com>
2024-01-30 23:41:56 +0000

    CV page

---
+++ src/app/cv/page.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import Section from '@/components/home/Section'
+import CvEntry from '@/components/home/CVEntry'
+
+function CV() {
+    return (
+        <div>
+            <Section title="Experience" subtitle="Previous roles">
+
+
+                <CvEntry
+                    institution="Commercial law firm, Glasgow"
+                    title="Trainee Solicitor"
+                    period="2022 - Current"
+                />
+
+                <CvEntry
+                    institution="Debt recovery law firm, Glasgow"
+                    title="Court Department / Developer"
+                    period="2016; 2019"
+                />
+
+            </Section>
+
+            <Section title="Education" subtitle="My background">
+
+                <CvEntry
+                    institution="University of Glasgow, UK"
+                    title="Diploma in Professional Legal Practise"
+                    result="Merit"
+                    period="2021 - 2022"
+                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ratione beatae accusantium quibusdam cumque quod aspernatur. Tenetur temporibus possimus expedita maiores voluptatum maxime, facere velit earum repellendus consectetur doloribus voluptates."
+                />
+
+                <CvEntry
+                    title="Law LLB (Hons.)"
+                    period="2017 - 2021"
+                    result="First class"
+                    description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ratione beatae accusantium quibusdam cumque quod aspernatur. Tenetur temporibus possimus expedita maiores voluptatum maxime, facere velit earum repellendus consectetur doloribus voluptates."
+                />
+
+                <CvEntry
+                    institution="Uppsala University, Sweden"
+                    title="Erasmus+ Exchange Programme (Law)"
+                    period="2019 - 2020"
+
+                />
+
+                <CvEntry
+                    institution="Unnamed High School"
+                    title="6x Scottish Highers; Advanced Higher Computing Science"
+                    result="AAAAAAB"
+                    period="2011 - 2017"
+
+                />
+
+            </Section>
+        </div>
+    )
+}
+
+export default CV
+
