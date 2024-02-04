90f75eec38f32de9eada08a66507bd69773d6fd8
Dan <36828197+dangraham99@users.noreply.github.com>
2024-02-04 17:04:19 +0000

    refactoring file locations

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import Section from '@/components/home/Section'
+import Section from '@/components/util/Section'
 
 function Contact() {
     return (
