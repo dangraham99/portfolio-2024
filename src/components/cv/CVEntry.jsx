c3c3d4b1be2f9a1660c08d57fe83f9866dadf75b
Dan <36828197+dangraham99@users.noreply.github.com>
2024-02-24 19:03:31 +0000

    change to period design

--- src/components/cv/CVEntry.jsx
+++ src/components/cv/CVEntry.jsx
@@ -5,9 +5,9 @@ function CvEntry(props) {
         <div className=''>
             <h2 className='font-semibold text-red-500 dark:text-red-400'>{props.institution}</h2>
             <div className='mb-8'>
-                <div class="flex xl:justify-between xl:flex-row flex-col mb-4">
+                <div class="flex-col mb-4">
                     <h2 className='font-semibold'>{props.title}<br></br><i>{props.result}</i></h2>
-                    <div className="my-2 flex items-center bg-zinc-300 dark:bg-zinc-800 px-3 text-sm shrink-0 rounded-3xl h-6 w-fit"><span className='uppercase font-semibold text-xs tracking-[.2em] '>{props.period}</span></div>
+                    <div className="my-2 flex items-center text-stone-800 dark:text-stone-400 text-sm shrink-0 rounded-3xl h-6 w-fit"><span className='uppercase  text-xs tracking-[.2em] '>{props.period}</span></div>
                 </div>
                 <div className='text-justify text-stone-800 dark:text-stone-400'>
                     {props.description}
