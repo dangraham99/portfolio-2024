d69749762d3c414d0082d9139dfc8e08fc1f2e2d
Dan <36828197+dangraham99@users.noreply.github.com>
2024-03-10 15:33:36 +0000

    adding pill design for result on CV page

--- src/components/cv/CVEntry.jsx
+++ src/components/cv/CVEntry.jsx
@@ -6,7 +6,14 @@ function CvEntry(props) {
             <h2 className='font-semibold text-stone-900 dark:text-stone-100'>{props.institution}</h2>
             <div className='mb-8'>
                 <div class="flex-col mb-4">
-                    <h2 className='font-semibold'>{props.title}<br></br><i>{props.result}</i></h2>
+                    <div>
+                        <h2 className='font-semibold'>{props.title}</h2>
+                        {props.result && <>
+
+                            <div className='mt-2 text-sm text-stone-500 '><span className='rounded-full px-3 py-1 bg-stone-700 dark:bg-stone-400 text-stone-100 dark:text-stone-900 '>{props.result}</span></div>
+
+                        </>}
+                    </div>
                     <div className="my-2 flex items-center text-stone-700 dark:text-stone-400 text-sm shrink-0 rounded-3xl h-6 w-fit"><span className='uppercase  text-xs tracking-[.2em] '>{props.period}</span></div>
                 </div>
                 <div className='text-justify text-stone-700 dark:text-stone-400'>
